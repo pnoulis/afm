eb8266e0d3d5bae1de8c58228a5eed6ad185708f
pnoul <pavlos.noulis@gmail.com>
2023-07-14 10:05:56 +0300

    Player entity initial capability ready

--- src/entities/player/Player.js
+++ src/entities/player/Player.js
@@ -1,67 +1,48 @@
-import { eventful } from "../../misc/eventful.js";
-import { stateful } from "../../misc/stateful.js";
+import { eventful } from "js_utils/eventful";
+import { stateful } from "js_utils/stateful";
 import { Unregistered } from "./StateUnregistered.js";
 import { Registered } from "./StateRegistered.js";
 import { InTeam } from "./StateInTeam.js";
 import { InGame } from "./StateInGame.js";
-// import { Wristband } from "../wristband/Wristband.js";
+import { PlayerWristband } from "../wristband/PlayerWristband.js";
 
 class Player {
-  static initialize(player, config) {
-    player.name = config.name || "";
-    player.username = config.username || "";
-    player.email = config.email || "";
-    player.password = config.password || "";
-    player.wristbandMerged = config.wristbandMerged ?? false;
-    // player.wristband = new Wristband(config.wristband);
-
-    if (config.inGame) {
-      player.setState(player.getInGameState);
-    } else if (config.wristbandMerged) {
-      player.setState(player.getInTeamState);
-    } else if (config.registered) {
-      player.setState(player.getRegisteredState);
+  constructor(player = {}) {
+    // Stateful initialization
+    stateful.construct.call(this);
+    // Player initialization
+    this.name = player.name || "";
+    this.username = player.username || "";
+    this.email = player.email || "";
+    this.password = player.password || "";
+    this.wristband = new PlayerWristband(player.wristband);
+    if (player.inGame) {
+      this.setState(this.getInGameState);
+    } else if (player.inTeam) {
+      this.setState(this.getInTeamState);
+    } else if (player.registered) {
+      this.setState(this.getRegisteredState);
     } else {
-      player.setState(player.getUnregisteredState);
+      this.setState(this.getUnregisteredState);
     }
   }
-
-  constructor(player = {}) {
-    Object.assign(
-      this,
-      stateful.call(this, {
-        unregistered: new Unregistered(this),
-        registered: new Registered(this),
-        inTeam: new InTeam(this),
-        inGame: new InGame(this),
-      })
-    );
-
-    Object.assign(
-      this,
-      eventful.call(this, {
-        stateChange: [],
-        unregistered: [],
-        registered: [],
-        inTeam: [],
-        inGame: [],
-      })
-    );
-    Player.initialize(this, player);
-  }
-  changeState(state, cb) {
-    const currentState = this.state.name;
-    this.setState(state, () => {
-      this.emit("stateChange", this.state.name, currentState);
-      cb && cb();
-    });
-  }
-
-  pairWristband() {
-    return new Promise((resolve, reject) => {
-      this.state.pairWristband(resolve, reject);
-    });
-  }
 }
+
+// Stateful
+stateful(Player, [
+  Unregistered,
+  "unregistered",
+  Registered,
+  "registered",
+  InTeam,
+  "inTeam",
+  InGame,
+  "inGame",
+]);
+
+// Eventful
+eventful(Player, {
+  stateChange: [],
+});
 
 export { Player };
