eca4b63c02a57a4d80d80d9185b14b3d6f4460f3
pavlos noulis <pavlos.noulis@gmail.com>
2023-09-19 05:59:08 +0300

    removed console.log statements

--- src/routes/checkSession.js
+++ src/routes/checkSession.js
@@ -3,7 +3,6 @@ function checkSession() {
     "check/session",
     async (context, next) => {
       const global = context.afmachine.services.storage.global.get(context);
-      console.log(global);
       context.res = {
         payload: {
           msg: "whoetnuh",
